786b336ea8265d41c5a051f297849f572285f177
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-03-29 19:55:41 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,13 @@
+==============
+tonttu.locales
+==============
+
+tonttu.locales overrides default translations of Plone for Tonttu site.
+
+Change log
+----------
+
+0.1 (2013-03-xx)
+================
+
+- Initial release. [taito]
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='tonttu.locales',
+    version='0.0',
+    description="Overrides default translations of Plone for ABITA site.",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@abita.fi',
+    url='https://github.com/taito/tonttu.locales',
+    license='None-free',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['tonttu'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.CMFPlone',
+        'setuptools'],
+    entry_points="""
+    # -*- Entry points: -*-
+
+    [z3c.autoinclude.plugin]
+    target = plone
+    """)
